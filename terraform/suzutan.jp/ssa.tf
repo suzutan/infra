63723a60b9260b15b853b95deaf03e5ffaf13c97
suzutan <6679870+suzutan@users.noreply.github.com>
2023-04-12 15:41:53 +0900

    remap ip

--- terraform/suzutan.jp/ssa.tf
+++ terraform/suzutan.jp/ssa.tf
@@ -3,30 +3,23 @@ resource "cloudflare_record" "ssa_sv_nas" {
   zone_id = data.cloudflare_zone.domain.id
   name    = "nas.sv.ssa"
   type    = "A"
-  value   = "172.20.1.3"
+  value   = "172.20.0.3"
   proxied = false
   ttl     = 1
 }
-resource "cloudflare_record" "ssa_cl_nas" {
-  zone_id = data.cloudflare_zone.domain.id
-  name    = "nas.cl.ssa"
-  type    = "A"
-  value   = "172.20.0.221"
-  proxied = false
-  ttl     = 1
-}
+
 resource "cloudflare_record" "ssa_sv_freesia" {
   zone_id = data.cloudflare_zone.domain.id
   name    = "freesia.sv.ssa"
   type    = "A"
-  value   = "172.20.1.4"
+  value   = "172.20.0.4"
   proxied = false
   ttl     = 1
 }
 
 resource "cloudflare_record" "ssa_cl_ap" {
   zone_id = data.cloudflare_zone.domain.id
-  name    = "ap.cl.ssa"
+  name    = "ap.sv.ssa"
   type    = "A"
   value   = "172.20.0.253"
   proxied = false
@@ -35,7 +28,7 @@ resource "cloudflare_record" "ssa_cl_ap" {
 
 resource "cloudflare_record" "ssa_cl_qsw" {
   zone_id = data.cloudflare_zone.domain.id
-  name    = "qsw.cl.ssa"
+  name    = "qsw.sv.ssa"
   type    = "A"
   value   = "172.20.0.253"
   proxied = false
