8f846dfa9d6e3a5cce6e58098ee707b365333599
suzutan <6679870+suzutan@users.noreply.github.com>
2024-02-06 05:26:42 +0900

    fix

--- terraform/harvestasya.org/lan_ssa.tf
+++ terraform/harvestasya.org/lan_ssa.tf
@@ -6,11 +6,12 @@ resource "cloudflare_record" "wildcard" {
   proxied = false
   ttl     = 1
 }
+
 resource "cloudflare_record" "adguard" {
   zone_id = data.cloudflare_zone.domain.id
   name    = "adguard"
-  type    = "A"
-  value   = "172.20.0.200"
+  type    = "CNAME"
+  value   = "adguard.sv.ssa.suzutan.jp."
   proxied = false
   ttl     = 1
 }
