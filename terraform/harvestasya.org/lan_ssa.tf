28744723689cdb0d33087fbac949d41405dcd90b
suzutan <6679870+suzutan@users.noreply.github.com>
2024-02-06 05:23:03 +0900

    add adguard record

--- terraform/harvestasya.org/lan_ssa.tf
+++ terraform/harvestasya.org/lan_ssa.tf
@@ -6,3 +6,11 @@ resource "cloudflare_record" "wildcard" {
   proxied = false
   ttl     = 1
 }
+resource "cloudflare_record" "adguard" {
+  zone_id = data.cloudflare_zone.domain.id
+  name    = "adguard"
+  type    = "A"
+  value   = "172.20.0.200"
+  proxied = false
+  ttl     = 1
+}
