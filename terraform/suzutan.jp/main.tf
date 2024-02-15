f9f3b42268a6a469593eb936892a691d07debb42
suzutan <6679870+suzutan@users.noreply.github.com>
2024-02-16 02:48:59 +0900

    fix cloudflare api_token(suzutan.jp)

--- terraform/suzutan.jp/main.tf
+++ terraform/suzutan.jp/main.tf
@@ -11,6 +11,10 @@ locals {
   account_id = "3b433b4bc6b6f9770dbf88886748ec8b"
 }
 
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+}
+
 data "cloudflare_zone" "domain" {
   name = local.domain
 }
