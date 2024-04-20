8536a628a8cf0ca08341c3807a116275375ccdc6
suzutan <6679870+suzutan@users.noreply.github.com>
2024-04-21 06:14:09 +0900

    create provider.tf

--- terraform/harvestasya.org/main.tf
+++ terraform/harvestasya.org/main.tf
@@ -5,19 +5,6 @@ locals {
   freesia_ddns_domain = "ddns.harvestasya.org"
 }
 
-provider "github" {
-  owner = local.owner
-  app_auth {
-    id              = var.app_id
-    installation_id = var.app_installation_id
-    pem_file        = var.app_pem_file
-  }
-}
-
-provider "cloudflare" {
-  api_token = var.cloudflare_api_token
-}
-
 data "github_organization" "main" {
   name = local.owner
 }
