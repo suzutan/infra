d4ff2d45c0657d4a06a404b2db05c43317ea7c28
suzutan <6679870+suzutan@users.noreply.github.com>
2024-04-21 06:16:37 +0900

    create locals.tf

---
+++ terraform/harvestasya.org/locals.tf
@@ -0,0 +1,6 @@
+
+locals {
+  owner               = "harvestasya"
+  zone_name           = "harvestasya.org"
+  freesia_ddns_domain = "ddns.harvestasya.org"
+}
--- terraform/harvestasya.org/main.tf
+++ terraform/harvestasya.org/main.tf
@@ -1,13 +1,4 @@
 
-locals {
-  owner               = "harvestasya"
-  zone_name           = "harvestasya.org"
-  freesia_ddns_domain = "ddns.harvestasya.org"
-}
-
-data "github_organization" "main" {
-  name = local.owner
-}
 
 data "cloudflare_zone" "domain" {
   name = local.zone_name
