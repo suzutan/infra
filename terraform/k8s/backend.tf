fa8ec361e83390f1e91b98d406672d275806d210
suzutan <6679870+suzutan@users.noreply.github.com>
2024-05-27 04:27:02 +0900

    remove libvirt k8s

--- terraform/k8s/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "suzutan-infra"
-    key    = "terraform/k8s"
-    region = "ap-northeast-3"
-  }
-}
--- terraform/k8s/variables.tf
+++
@@ -1,5 +0,0 @@
-
-# variable "cloudflare_api_token" {
-#   type        = string
-#   description = "API token for Cloudflare"
-# }
