a7e13c4a047d0c0c3cf2671bcd16701a884d697a
suzutan <6679870+suzutan@users.noreply.github.com>
2025-03-07 02:07:40 +0900

    delete

--- terraform/suzutan.jp/record_hatenablog.tf
+++
@@ -1,9 +0,0 @@
-
-resource "cloudflare_record" "blog" {
-  zone_id = data.cloudflare_zone.domain.id
-  name    = "blog"
-  type    = "CNAME"
-  ttl     = "1"
-  proxied = "false"
-  content = "hatenablog.com"
-}
