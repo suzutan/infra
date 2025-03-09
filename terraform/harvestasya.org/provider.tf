62a76bec617226beed92d736175347f9a3dced7a
suzutan <6679870+suzutan@users.noreply.github.com>
2025-03-09 23:41:20 +0900

    upgrade harvestasya.org

--- terraform/harvestasya.org/provider.tf
+++ terraform/harvestasya.org/provider.tf
@@ -1,12 +1,4 @@
 
-provider "github" {
-  owner = local.owner
-  app_auth {
-    id              = var.app_id
-    installation_id = var.app_installation_id
-    pem_file        = var.app_pem_file
-  }
-}
 
 provider "cloudflare" {
   api_token = var.cloudflare_api_token
