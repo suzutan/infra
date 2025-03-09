2b5c413735ed218e248cd86585af75b8ca9cbb6d
suzutan <6679870+suzutan@users.noreply.github.com>
2025-03-10 00:30:48 +0900

    remove unnecessary config

--- terraform/harvestasya.org/variables.tf
+++ terraform/harvestasya.org/variables.tf
@@ -1,18 +1,3 @@
-variable "app_id" {
-  type        = number
-  description = "ID of the GitHub App"
-}
-
-variable "app_installation_id" {
-  type        = number
-  description = "Installation ID of the GitHub App"
-}
-
-variable "app_pem_file" {
-  type        = string
-  description = "Path to the private key file for the GitHub App"
-}
-
 variable "cloudflare_account_id" {
   type        = string
   description = "Account ID of Cloudflare"
@@ -21,14 +6,4 @@ variable "cloudflare_account_id" {
 variable "cloudflare_api_token" {
   type        = string
   description = "API token for Cloudflare"
-}
-
-variable "github_client_id" {
-  type        = string
-  description = "Client ID of the GitHub OAuth App"
-}
-
-variable "github_client_secret" {
-  type        = string
-  description = "Client secret of the GitHub OAuth App"
 }
