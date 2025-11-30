08f84729f701c6c679e70940cf5adbac789294d1
suzutan <6679870+suzutan@users.noreply.github.com>
2025-12-01 02:38:59 +0900

    fix: add ArgoCD IngressRoute for Traefik routing
    - Create IngressRoute for argocd.harvestasya.org
    - Add ServersTransport for TLS skip verification
    - Disable Helm chart's built-in cloudflare-tunnel Ingress

--- terraform/harvestasya.org/tunnel_web.tf
+++ terraform/harvestasya.org/tunnel_web.tf
@@ -1,5 +1,19 @@
 # Cloudflare Zero Trust Tunnel for Web traffic (Traefik)
-# ワイルドカード方式: 新規IngressRoute追加時にTerraform変更不要
+
+locals {
+  k8s_ingress_rules = [
+    "argocd.harvestasya.org",
+    "echoserver.harvestasya.org",
+    "grathnode.harvestasya.org",
+    "asf.harvestasya.org",
+    "navidrome.harvestasya.org",
+    "navidrome-filebrowser.harvestasya.org",
+    "influxdb2.harvestasya.org",
+    "grafana.harvestasya.org",
+    "prometheus.harvestasya.org",
+    "n8n.harvestasya.org",
+  ]
+}
 
 # Cloudflare Tunnelの作成
 resource "cloudflare_zero_trust_tunnel_cloudflared" "web_tunnel" {
@@ -7,37 +21,36 @@ resource "cloudflare_zero_trust_tunnel_cloudflared" "web_tunnel" {
   name       = "harvestasya symphonic-reactor"
 }
 
-# Tunnel設定 - すべての *.harvestasya.org をTraefikにルーティング
+# Tunnel設定 - Traefikにルーティング
 resource "cloudflare_zero_trust_tunnel_cloudflared_config" "web_tunnel_config" {
   account_id = var.cloudflare_account_id
   tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.web_tunnel.id
 
   config = {
-    ingress = [
-      {
-        hostname = "*.${local.zone_name}"
-        service  = "https://traefik.traefik.svc.cluster.local"
-        origin_request = {
-          no_tls_verify = true
-        }
-      },
-      {
-        # Catch-all (required by cloudflared)
-        service = "http_status:404"
+    ingress = concat([for ingress_rule in local.k8s_ingress_rules : {
+
+      hostname = ingress_rule
+      service  = "https://traefik.traefik.svc.cluster.local"
+      origin_request = {
+        origin_server_name = ingress_rule
+        http_host_header   = ingress_rule
+        no_tls_verify      = true
       }
-    ]
+      }],
+      [{
+        service = "http_status:404"
+    }])
   }
 }
 
-# DNS ワイルドカードCNAMEレコード
-resource "cloudflare_dns_record" "web_tunnel_wildcard" {
-  zone_id = local.zone_id
-  name    = "*"
-  content = "${cloudflare_zero_trust_tunnel_cloudflared.web_tunnel.id}.cfargotunnel.com"
-  ttl     = 1
-  type    = "CNAME"
-  proxied = true
-  comment = "Cloudflare Tunnel wildcard for K8s Traefik"
+resource "cloudflare_dns_record" "k8s_ingress" {
+  for_each = toset(local.k8s_ingress_rules)
+  zone_id  = local.zone_id
+  name     = each.value
+  content  = "${cloudflare_zero_trust_tunnel_cloudflared.web_tunnel.id}.cfargotunnel.com"
+  ttl      = 1
+  type     = "CNAME"
+  proxied  = true
 }
 
 # Output: トンネルトークン (Kubernetes cloudflared設定に使用)
