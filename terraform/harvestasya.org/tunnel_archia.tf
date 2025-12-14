b233123cebc55f7f401c396996f46df8fb63baa3
suzutan <6679870+suzutan@users.noreply.github.com>
2025-12-14 14:58:49 +0900

    tunnel再設定

---
+++ terraform/harvestasya.org/tunnel_archia.tf
@@ -0,0 +1,56 @@
+
+resource "cloudflare_zero_trust_tunnel_cloudflared" "archia" {
+  account_id = var.cloudflare_account_id
+  name       = "Archia SSH"
+}
+
+resource "cloudflare_zero_trust_tunnel_cloudflared_config" "archia" {
+  account_id = var.cloudflare_account_id
+  tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.archia.id
+
+  config = {
+    ingress = [{
+      hostname = "archia.${local.zone_name}"
+      service  = "ssh://localhost:22"
+      },
+      {
+        service = "http_status:404"
+    }]
+  }
+}
+
+resource "cloudflare_dns_record" "archia" {
+  zone_id = local.zone_id
+  name    = "archia"
+  ttl     = "1"
+  content = "${cloudflare_zero_trust_tunnel_cloudflared.archia.id}.cfargotunnel.com"
+  type    = "CNAME"
+  proxied = true
+  comment = "SSH access to Archia"
+}
+
+# Access Application
+resource "cloudflare_zero_trust_access_application" "archia" {
+  account_id                = var.cloudflare_account_id
+  name                      = "Archia SSH"
+  domain                    = "archia.${local.zone_name}"
+  type                      = "self_hosted"
+  session_duration          = "24h"
+  auto_redirect_to_identity = true
+  allowed_idps              = [cloudflare_zero_trust_access_identity_provider.github.id]
+
+  policies = [
+    {
+      id = cloudflare_zero_trust_access_policy.infrastructure_admin.id
+    }
+  ]
+}
+
+# Outputs
+output "archia_tunnel_id" {
+  value = cloudflare_zero_trust_tunnel_cloudflared.archia.id
+}
+
+output "archia_hostname" {
+  value = "archia.${local.zone_name}"
+}
---
+++ terraform/harvestasya.org/tunnel_harvestasya.tf
@@ -0,0 +1,85 @@
+resource "cloudflare_zero_trust_tunnel_cloudflared" "harvestasya" {
+  account_id = var.cloudflare_account_id
+  name       = "Harvestasya"
+}
+
+resource "cloudflare_zero_trust_tunnel_cloudflared_config" "harvestasya" {
+  account_id = var.cloudflare_account_id
+  tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.harvestasya.id
+
+  config = {
+    ingress = [
+      {
+        hostname = "tyria.${local.zone_name}"
+        service  = "ssh://localhost:22"
+      },
+      {
+        hostname = "vista.${local.zone_name}"
+        service  = "https://localhost:8006"
+        origin_request = {
+          no_tls_verify = true # pveの自己署名証明書を許容する
+        }
+      },
+      {
+        service = "http_status:404" # catch-all
+      }
+    ]
+  }
+}
+
+resource "cloudflare_dns_record" "vista" {
+  zone_id = local.zone_id
+  name    = "vista"
+  content = "${cloudflare_zero_trust_tunnel_cloudflared.harvestasya.id}.cfargotunnel.com"
+  ttl     = 1
+  type    = "CNAME"
+  proxied = true
+  comment = "Harvestasya Vista (Proxmox VE Web UI)"
+}
+
+resource "cloudflare_dns_record" "tyria" {
+  zone_id = local.zone_id
+  name    = "tyria"
+  content = "${cloudflare_zero_trust_tunnel_cloudflared.harvestasya.id}.cfargotunnel.com"
+  ttl     = 1
+  type    = "CNAME"
+  proxied = true
+  comment = "Tyria (Proxmox VE SSH)"
+}
+
+resource "cloudflare_zero_trust_access_application" "vista" {
+  account_id                = var.cloudflare_account_id
+  name                      = "Harvestasya Vista (Proxmox VE)"
+  domain                    = "vista.${local.zone_name}"
+  type                      = "self_hosted"
+  session_duration          = "24h"
+  auto_redirect_to_identity = true
+  allowed_idps              = [cloudflare_zero_trust_access_identity_provider.github.id]
+
+  policies = [
+    {
+      id = cloudflare_zero_trust_access_policy.infrastructure_admin.id
+    }
+  ]
+}
+
+resource "cloudflare_zero_trust_access_application" "tyria" {
+  account_id                = var.cloudflare_account_id
+  name                      = "Tyria (Proxmox VE SSH)"
+  domain                    = "tyria.${local.zone_name}"
+  type                      = "self_hosted"
+  session_duration          = "24h"
+  auto_redirect_to_identity = true
+  allowed_idps              = [cloudflare_zero_trust_access_identity_provider.github.id]
+
+  policies = [
+    {
+      id = cloudflare_zero_trust_access_policy.infrastructure_admin.id
+    }
+  ]
+}
+
+output "vista_tunnel_id" {
+  value       = cloudflare_zero_trust_tunnel_cloudflared.harvestasya.id
+  description = "Harvestasya Vista Tunnel ID"
+}
