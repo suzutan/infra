2badb44334e4ef546240c9a5c6b4b54b07c13cab
suzutan <6679870+suzutan@users.noreply.github.com>
2025-12-27 16:06:18 +0900

    update acme IP

--- terraform/harvestasya.org/dns.tf
+++ terraform/harvestasya.org/dns.tf
@@ -2,7 +2,7 @@
 resource "cloudflare_dns_record" "acme" {
   zone_id = local.zone_id
   name    = "acme"
-  content = "172.20.2.202"
+  content = "10.11.0.202" #step-ca svc/step-certificates-acme
   ttl     = 1
   type    = "A"
   proxied = false # Direct DNS resolution (no Cloudflare proxy)
